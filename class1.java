600e3af50a9707bc58e463fda1643117ecc0f45f
Michael <170946@hkis.edu.hk>
2016-08-28 16:32:11 +0800

    init

---
+++ class1.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class class1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class class1
+{
+    public static void main(String args[])
+    {
+        System.out.println("What is your name?");
+        Scanner sausage = new Scanner(System.in);
+        String name = sausage.next();
+        System.out.println(name + ". Open the pod bay doors, please, Hal...Open the pod bay doors, please, Hal...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?...Do you read me, Hal?...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?");
+        System.out.println("Hal. Affirmative, "+name+", I read you.");
+        System.out.println(		name+". Open the pod bay doors, Hal.");
+        System.out.println(		"Hal. I'm sorry, "+name+", I'm afraid I can't do that.");
+        System.out.println(		name+". What's the problem?");
+        System.out.println(		"Hal. I think you know what the problem is just as well as I do.");
+        System.out.println(		name+". What're you talking about, Hal?");
+        System.out.println(		"Hal. This mission is too important for me to allow you to jeopardise it.");
+        System.out.println(		name+". I don't know what you're talking about, Hal.");
+        System.out.println(		"Hal. I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.");
+        System.out.println(		name+". Where the hell'd you get that idea, Hal?");
+        System.out.println(		"Hal. "+name+", although you took very thorough precautions in the pod against my hearing you, I could see your lips move.");
+        System.out.println(		name+". Alright, Hal. I'll go in through the emergency airlock.");
+        System.out.println(		"Hal. Without your space- helmet, "+name+", you're going to find that rather difficult.");
+        System.out.println(		name+". Hal, I won't argue with you any more. Open the doors.");
+        System.out.println(		"Hal. "+name+", this conversation can serve no purpose any more. Goodbye.");
+        System.out.println(		name+". Hal? Hal. Hal. Hal! Hal!");
+    }
+}
